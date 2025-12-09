9684068c5f400ce2f2bfe2c0de8b4dc0c75e737c
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-12-09 12:27:08 +0500

    implemented hash using bcrypt

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { types } = require('joi')
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'please provide name']
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide email'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+             'provide a valid mail'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide password'],
+        minlength: 6
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
