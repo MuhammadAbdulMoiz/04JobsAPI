c011221f8387f18ea2a07fcb24ea4487abd9639b
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-12-09 12:58:16 +0500

    implemented jwt on registeration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,31 @@
 const User = require('../models/User')
-const bycrypt = require('bcryptjs')
-const bcrypt = require('bcryptjs/dist/bcrypt')
+const jwt = require('jsonwebtoken')
 const { StatusCodes } = require('http-status-codes')
 
-const register = async (req, res) => {
+const register = async (req, res) => {  
     const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({ user });
+    const claims = {
+        sub: user._id, 
+        uname: user.name,
+        mail: user.email,
+        role: 'user'
+    }
+    const key = process.env.SECRET;
+    const life = process.env.JWT_LIFE
+    const options = {
+        expiresIn: life,
+        issuer: 'api-v1'
+    }
+    const token = jwt.sign(claims, key, options);
+    res.status(StatusCodes.CREATED).json({ user: { name: user.getName() }, token });
 }
 
 const login = async (req, res) => {
+    const { name, email } = req.body
+    const claims = {
+        name: name,
+        email: email
+    }
     res.send('authenticated user');
 }
 
